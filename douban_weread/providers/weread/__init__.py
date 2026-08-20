042de45bc87fefc5d26f01c6573216854f9216f7
Linlu Li <122972460+LCubed101@users.noreply.github.com>
2026-08-20 19:02:59 +0800

    feat: add official WeRead provider package

---
+++ douban_weread/providers/weread/__init__.py
@@ -0,0 +1,13 @@
+from .client import (
+    WeReadAuthError,
+    WeReadClient,
+    WeReadProviderError,
+    WeReadSearchCandidate,
+)
+
+__all__ = [
+    "WeReadAuthError",
+    "WeReadClient",
+    "WeReadProviderError",
+    "WeReadSearchCandidate",
+]
